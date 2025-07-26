268773a4c84fb15f63dfca7fcd3eb117a34bea25
TNet <158599958+githur1234@users.noreply.github.com>
2025-07-26 09:19:40 +0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pip install -r requirements.txt
+CURRENT_DIR="$(pwd)"
+SHELL_NAME=$(basename "$SHELL")
+if [ "$SHELL_NAME" = "bash" ]; then
+  RC_FILE="$HOME/.bashrc"
+elif [ "$SHELL_NAME" = "zsh" ]; then
+  RC_FILE="$HOME/.zshrc"
+else
+  echo "Your shell is not zsh or bash. Skipping global installation of fatsh."
+  exit 1
+fi
+echo "export PATH=\"\$PATH:$CURRENT_DIR\"" >> "$RC_FILE"
+source "$RC_FILE"
+python3 fatsh.py
